3c97d7c046e657b67d05f8694ea987adc61da01e
stefan-scott <scottst@spsd.sk.ca>
2026-01-12 10:35:48 -0600

    Create 21 Microbit Image Functions.py

---
+++ 21 Microbit Image Functions.py
@@ -0,0 +1,77 @@
+# Microbit Image Functions
+# Mr .Scott
+# January 8, 2026
+
+import microbit, time, random
+
+# grid[3][4]
+# grid[y][x]
+
+
+# ----------------------------------
+#    LED Library CODE         
+# ----------------------------------
+
+grid = [ [0, 0, 0, 0, 0 ],
+         [0, 0, 0, 0, 0 ],
+         [0, 0, 0, 0, 0 ],
+         [0, 0, 0, 0, 0 ],
+         [0, 0, 0, 0, 0 ] ]
+
+def print_grid():
+    #print out the grid in an easy-to-read format
+    for row in grid:
+        print(row)
+
+def show_grid():
+    #convert our 2D list to a string, and display
+    result = ""
+    for row in grid:
+        for pixel in row:
+            result += str(pixel)
+        result += ":"
+        
+    result = result[0:-1]
+    print(result)
+    microbit.display.show(microbit.Image(result))
+
+def set_pixel(x, y, intensity):
+    #set a pixel at (x,y) to a brightness (0-9)
+    grid[y][x] = intensity
+    show_grid()
+
+def plot(x,y):
+    #pixel at (x,y) to full brightness
+    set_pixel(x,y,9)
+
+def unplot(x,y):
+    #turn off led at (x,y)
+    set_pixel(x,y,0)
+
+def queue_pixel(x, y, intensity):
+    #modify a pixel, but don't display yet...
+    grid[y][x] = intensity
+
+def set_all(intensity):
+    # intensity → INTEGER representing brightness (0-9)
+    # turn on all LEDs
+    for x in range(5):      #0, 1, 2, 3, 4
+        for y in range(5):  #0, 1, 2, 3, 4
+            queue_pixel(x,y,intensity)
+    show_grid()
+    
+# ------------------------------------------------------
+#                   Library Ends Here
+# ------------------------------------------------------
+
+# Fading Screen Animation
+# 0 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
+for i in range(5):
+    for brightness in range(10):
+        set_all(brightness)
+        time.sleep(0.005)
+
+    for brightness in range(8, 0, -1):
+        set_all(brightness)
+        time.sleep(0.005)
+set_all(0)
